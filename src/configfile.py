6a3c172d9f476629e89d68592c91178265173d1d
Zan_23 <zasonce@gmail.com>
2021-10-21 14:16:45 +0200

    Fixed conf

--- src/configfile.py
+++ src/configfile.py
@@ -35,7 +35,29 @@ PUMPS_TUPLES = [("braila_flow211206H360.csv", "Jonctiune-3974"),
                 ("braila_flow318505H498.csv", "Jonctiune-J-19"),
                 ("braila_flow211306H360.csv", "Jonctiune-2749")
                 ]
+# PUMPS_TUPLES = [("braila_flow211206H360.csv", "Apollo"),
+#                 ("braila_flow211106H360.csv", "GA-Braila"),
+#                 ("braila_flow318505H498.csv", "RaduNegru 2"),
+#                 ("braila_flow211306H360.csv", "RaduNegruMare")
+#                 ]
 
+
+# # KAFKA
+# order in array is important !!, since it is mapped by index
+KAFKA_NODES_ORDER = ["Jonctiune-3974", "Jonctiune-2749", "Jonctiune-J-19", "SenzorComunarzi-NatVech",
+                     "SenzorComunarzi-castanului", "SenzorChisinau-Titulescu", "SenzorCernauti-Sebesului"]
+
+# KAFKA related
+HOST_AND_PORT = "194.249.231.11:9092"
+
+TOPIC = "features_braila_leakage_detection"
+OUTPUT_TOPIC = "braila_leakage_groups"
+
+OUTPUT_JSON_NODES_KEY = "data"
+OUTPUT_JSON_TIME_KEY = "timestamp"
+
+
+# OUTDATED
 # The current tuples are made from the mail (Marius)
 # almost correct but these were not provided by CUP Braila
 # "braila_flow211206H360.csv"] = "748-B"      # ("Apollo", "748-B"), ,
@@ -46,20 +68,6 @@ PUMPS_TUPLES = [("braila_flow211206H360.csv", "Jonctiune-3974"),
 # # Nodes which to keep in the dataframes
 # SELECTED_NODES = ["SenzorComunarzi-NatVech", "SenzorCernauti-Sebesului", "SenzorChisinau-Titulescu",
 #                   "SenzorComunarzi-castanului", "Jonctiune-3974", "Jonctiune-J-3", "Jonctiune-J-19", "Jonctiune-2749"]
-
-
-# order in array is important !!, since it is mapped by index
-KAFKA_NODES_ORDER = ["Jonctiune-J-3", "Jonctiune-3974", "Jonctiune-2749", "Jonctiune-J-19", "SenzorComunarzi-NatVech",
-                     "SenzorComunarzi-castanului", "SenzorChisinau-Titulescu", "SenzorCernauti-Sebesului"]
-# # TODO newest order - uncomment and replace
-# KAFKA_NODES_ORDER = ["Jonctiune-3974", "Jonctiune-2749", "Jonctiune-J-19", "SenzorComunarzi-NatVech",
+#
+# KAFKA_NODES_ORDER = ["Jonctiune-J-3", "Jonctiune-3974", "Jonctiune-2749", "Jonctiune-J-19", "SenzorComunarzi-NatVech",
 #                      "SenzorComunarzi-castanului", "SenzorChisinau-Titulescu", "SenzorCernauti-Sebesului"]
-
-# KAFKA related
-HOST_AND_PORT = "194.249.231.11:9092"
-
-TOPIC = "features_braila_leakage_detection"
-OUTPUT_TOPIC = "braila_leakage_groups"
-
-OUTPUT_JSON_NODES_KEY = "data"
-OUTPUT_JSON_TIME_KEY = "timestamp"
