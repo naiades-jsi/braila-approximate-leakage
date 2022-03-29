e186f47bdbf2296f5d1f196fece2e5271be11533
Zan_23 <zasonce@gmail.com>
2022-03-29 16:34:09 +0200

    Simulation generation code fixes

--- data_generation_delft/Main2.py
+++ data_generation_delft/Main2.py
@@ -1,40 +1,35 @@
-#%% 
+# %%
 from EPANETTools import *
 from FileTools import *
 import wntr
 import sys
 import pickle
 
-
-#%%
-RunId=int(sys.argv[1])
-DirInput = sys.argv[2]+'/'
+# %%
+RunId = int(sys.argv[1])
+DirInput = sys.argv[2] + '/'
 DirOutput = sys.argv[3]
-FileInp=sys.argv[4]
-Mod=sys.argv[5]
+FileInp = sys.argv[4]
+Mod = sys.argv[5]
 
-#%%
+# %%
 print(f'Run number {RunId}')
 print(f'Path of input {DirInput}')
 print(f'Outputs are located in {DirOutput}')
 print(f'Input File Name {FileInp}')
 
-File = DirInput+FileInp #network_PDA.inp'  # File used for the model
+File = DirInput + FileInp  # network_PDA.inp'  # File used for the model
 print(File)
-Net1=wntr.network.WaterNetworkModel(File)    
-BD, MB, Lnz=BaseDemand(Net1) #Average Demand of the system in m3/s
-#Lnz will be were there is demand so is a sensor node
+Net1 = wntr.network.WaterNetworkModel(File)
+BD, MB, Lnz = BaseDemand(Net1)  # Average Demand of the system in m3/s
+# Lnz will be were there is demand so is a sensor node
 
-f=open(DirInput+'wn.pkl','wb')
-pickle.dump(Net1,f)
-f.close()
-Net1=RunNet(Net1,'')
-f=open(DirInput+'ns.pkl','wb')
-pickle.dump(Net1,f)
-f.close()
+with open(DirInput + "wn.pkl", "wb") as f:
+    pickle.dump(Net1, f)
 
-
+Net1 = RunNet(Net1, "")
+with open(DirInput + "ns.pkl", "wb") as f:
+    pickle.dump(Net1, f)
 
 print('Run cycle2')
-RunCycle2(Run=RunId,Threshold=0.5,DirPath=DirInput,Lnz=Lnz,ModName=Mod)
-
+RunCycle2_v2(Run=RunId, Threshold=0.5, DirPath=DirInput, Lnz=Lnz, ModName=Mod)
