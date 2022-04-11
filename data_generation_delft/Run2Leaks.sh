4cdef4a31d472a707b9f97f50bae031c4ae10c54
Zan_23 <zasonce@gmail.com>
2022-04-11 22:26:29 +0200

    Small fix

--- data_generation_delft/Run2Leaks.sh
+++ data_generation_delft/Run2Leaks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=NAIADES_CalGA
+#SBATCH --job-name=NAIADES_JOB_ZAN_11
 #SBATCH -N 4 --ntasks=50
 #SBATCH --time  24:00:00
 #SBATCH --output=outGA/NAIDES_%A_%a.out
@@ -7,6 +7,7 @@
 #SBATCH --partition thin
 #SBATCH --cpus-per-task 1
 #SBATCH --mem=4G
+#SBATCH --mail-user=stanonik
 #SBATCH --array=1-201
 module load 2021
 module load Python/3.9.5-GCCcore-10.3.0
@@ -25,6 +26,6 @@ echo "Running WDN Network - " $Mod
 echo 'Running WDN - ' ${NetworkRuns[$Mod]}
 echo "$SLURM_ARRAY_TASK_ID"
 
-cp  $HOME/NAIADES/Start/Networks/${NetworkRuns[$Mod]} "$TMPDIR" # TODO create file structure
-DataDirectory="/scratch-shared/NAIADES/$Mod"  # TODO add your folder
+cp  $HOME/NAIADES/Start/Networks/${NetworkRuns[$Mod]} "$TMPDIR"
+DataDirectory="/scratch-shared/NAIADES/$Mod"
 python Main2.py $SLURM_ARRAY_TASK_ID $TMPDIR $DataDirectory ${NetworkRuns[$Mod]} $Mod
