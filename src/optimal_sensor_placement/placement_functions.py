e95ac22ddaed5d811200e8eef7492266d32ffe92
Zan_23 <zasonce@gmail.com>
2022-04-26 13:21:32 +0200

    Optimal sensor placement - added placeholder package for functions for optimal sensor placement

---
+++ src/optimal_sensor_placement/placement_functions.py
@@ -0,0 +1,13 @@
+
+"""
+This file is meant as a place holder for the functions that could be implemented to provide a full functionality
+for the management of the water distribution networks.
+
+Functions implementing the following papers should be implemented here:
+- https://www.sciencedirect.com/science/article/pii/S1877705815026491
+- https://www.mdpi.com/1424-8220/22/2/443/pdf
+
+Functions should take in the EPANET network and locations of already existing sensors and return the optimal number of
+locations for the N sensors that are available for placement.
+"""
+
