90604744f313f242f9aee8d20fe0ec9b0444f41d
Zan_23 <zasonce@gmail.com>
2022-05-13 11:08:38 +0200

    Changed main_data_gen to have the correct parameters

--- data_generation/main_data_gen.py
+++ data_generation/main_data_gen.py
@@ -4,10 +4,10 @@ from data_generation.EpanetLeakGenerator import EpanetLeakGenerator
 if __name__ == "__main__":
     output_dir = "./one_leak_per_node_output"
     leak_generator_instance = EpanetLeakGenerator(epanet_file_name="./../data/epanet_networks/Braila_V2022_2_2.inp",
-                                                  number_of_threads=4,
+                                                  number_of_threads=6,
                                                   min_leak=0.5,
-                                                  max_leak=2.0,
-                                                  leak_flow_step=0.01,
+                                                  max_leak=23.1,
+                                                  leak_flow_step=0.001,
                                                   leak_flow_threshold=0.5,
                                                   output_dir=output_dir,
                                                   log_file=f"{output_dir}/data_generation.log")
